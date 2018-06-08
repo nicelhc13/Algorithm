c9ebb57a5d900ae3c5e9a01a53ec8a5c7b12037c
hochan <nicelhc13@naver.com>
2018-06-08 11:29:47 -0700

    algorithm: the maze makers (dfs)- add convertion between hex and bin

--- DFS/TheMazeMaker/maze.cpp
+++ DFS/TheMazeMaker/maze.cpp
@@ -4,8 +4,76 @@
 using namespace std;
 
 int h, w;
+vector<pair<int, int> > outedge;
 vector<vector<char> > mazes;
 
+string hexTobin(char c)
+{
+	switch(c)
+	{
+		case 'A':
+			return "1010";
+		case 'B':
+			return "1011";
+		case 'C':
+			return "1100";
+		case 'D':
+			return "1101";
+		case 'E':
+			return "1110";
+		case 'F':
+			return "1111";
+		case '0':
+			return "0000";
+		case '1':
+			return "0001";
+		case '2':
+			return "0010";
+		case '3':
+			return "0011";
+		case '4':
+			return "0100";
+		case '5':
+			return "0101";
+		case '6':
+			return "0110";
+		case '7':
+			return "0111";
+		case '8': 
+			return "1000";
+		case '9':
+			return "1001";
+		default:
+			return "fail";
+	}
+}
+
+int calc_out(int x, int y, int val)
+{
+	string bin = hexTobin(val);
+	if (x == h-1 && y == w-1) { // n, n
+		if (bin[1] == '0' || bin[2] == '0') return 1;
+	}  else if (0 < x && x < h && y == w-1) { // n-x, n
+		if (bin[1] == '0') return 1;
+	} else if (x == h-1 && 0 < y && y < w) { // n, n-x
+		if (bin[2] == '0') return 1;
+	} else if (x == 0 && y == 0) { // 0, 0
+		// 0 x x 0
+		if (bin[0] == '0' || bin[3] == '0') return 1;
+	} else if (x == 0 && y == w-1) { // 0, n
+		if (bin[0] == '0' || bin[1] == '0') return 1;
+	} else if (x == h-1 && y == 0) { // n, 0
+		if (bin[2] == '0' || bin[3] == '0') return 1;
+	} else if (0 < x && x < h && y == 0) { // n-x, 0
+		if (bin[3] == '0') return 1;
+	} else if (x == 0 && 0 < y && y < w) { // 0, n-y
+		if (bin[0] == '0') return 1;
+	}	
+
+	return 0;
+}
+
+/*
 int try_walking(int x, int y)
 {
 	if (x == y == dest) return
@@ -16,7 +84,7 @@ int try_walking(int x, int y)
 				ret += try_walking();
 
 	return ret;
-}
+}*/
 
 
 int main(void)
@@ -25,15 +93,18 @@ int main(void)
 		cin >> h >> w;
 		if (h == 0 && w == 0)	return 0;
 		mazes = vector<vector <char> >(h, vector<char>(w, 0));
-		for (int i = 0; i < h; i++)
-			for (int j = 0; j < w; j++)
-				cin >> mazes[i][j]; 
-
+		outedge.resize(0);
 		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++)
-				cout << mazes[i][j] << " ";
-			cout << endl;
+			for (int j = 0; j < w; j++) {
+				std::cin >> std::hex >> mazes[i][j]; 
+				if (calc_out(i, j, mazes[i][j])) {
+					outedge.push_back(make_pair(i, j));
+					cout << "added:" << mazes[i][j] << endl;
+				}
+			}
 		}
+
+		cout << "FINISH====" << endl;
 	}
 	return 0;
 }
